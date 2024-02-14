1b4955667840e1a0e2dc3d4df8ada681ed1a9891
salapakasreepranav <132810410+salapakasreepranav@users.noreply.github.com>
2024-02-14 18:51:45 +0530

    configured connection pooling by hikaricp (#1)

--- src/sp/sree/util/JdbcUtil.java
+++ src/sp/sree/util/JdbcUtil.java
@@ -1,11 +1,15 @@
 package sp.sree.util;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 
 public class JdbcUtil
 {
@@ -28,6 +32,20 @@ public class JdbcUtil
 	
 	public static Connection getJdbcConnection() throws IOException, SQLException
 	{
+		//return getPhysicalConnection(); //for normal connection (uncomment for normal connection and also change application.properties file)
+		return getLogicalConnection(); //for connection pooling
+	}
+	
+	private static Connection getLogicalConnection() throws FileNotFoundException, IOException, SQLException 
+	{
+		HikariConfig config = new HikariConfig("E:\\iNeuron\\Servlets\\Projects\\ProjectEMS\\src\\sp\\sree\\properties\\application.properties");
+		HikariDataSource dataSource = new HikariDataSource(config);
+		
+		
+		return dataSource.getConnection();
+	}
+
+	private static Connection getPhysicalConnection() throws FileNotFoundException, IOException, SQLException {
 		FileInputStream fis = new FileInputStream("E:\\iNeuron\\Servlets\\Projects\\ProjectEMS\\src\\sp\\sree\\properties\\application.properties");
 		Properties properties = new Properties();
 		properties.load(fis);
